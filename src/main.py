96317b033a430ee4374da4ad54827e3dd7546f5b
James Gleeson <courtney.gleeson@gmail.com>
2025-12-28 12:20:10 -0800

    add fastapi frontend to call power manager.

---
+++ src/main.py
@@ -0,0 +1,38 @@
+"""
+Quick rest api for the power manager.
+"""
+from fastapi import FastAPI
+from pydantic import BaseModel
+from power_man import PowerEventManager
+
+app = FastAPI()
+pow_manager = PowerEventManager()
+
+class PowerEvent(BaseModel):
+    """
+    Docstring for PowerEvent
+    """
+    name: str
+    lat: float
+    lon: float
+
+
+@app.get("/")
+async def root():
+    """
+    Docstring for root
+    """
+    return {"message": "Root of Power Manager API"}
+
+@app.post("/add_event/")
+async def add_event(event: PowerEvent):
+    """
+    Docstring for add_event
+    
+    :param event_name: Description
+    :type event_name: str
+    """
+    print(f"Received event: {event}")
+    json_str = event.model_dump_json()
+    pow_manager.add_power_event_json(json_str)
+    return {"message": f"Event {event.name} added successfully."}
